9643248d869d60ddb4c0cf088272e690d850c791
Bekir Faruk ARABACI <113997401+BekirFarukARABACI@users.noreply.github.com>
2024-03-21 21:58:18 +0300

    Feat : Kullanıcı Arayüzü eklendi

---
+++ app/src/main/java/com/bekirfarukarabaci/calculator/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.bekirfarukarabaci.calculator
+
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.bekirfarukarabaci.calculator.databinding.ActivityMainBinding
+import java.util.ArrayList
+
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+
+    }
+}
